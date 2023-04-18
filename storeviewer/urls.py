93b2b8733646753c3a71d64326fe98b0752ab4ca
Jacob Adams <jacob@tookmund.com>
2023-04-18 18:48:51 -0400

    Basic template setup

--- storeviewer/urls.py
+++ storeviewer/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import updateCerts
+from .views import homePage, updateCerts, viewStore, viewCert
 
 urlpatterns = [
+    path("", homePage),
     path("update", updateCerts, name="update"),
+    path("rootstore/<name>", viewStore),
+    path("cert/<sha256>", viewCert),
 ]
--- storeviewer/views.py
+++ storeviewer/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .fetch_rootstores import mozilla_update, microsoft_update
 
+from .models import Root_Store, Certificate, Store_Contents
+
 # Create your views here.
 
 
@@ -10,3 +12,25 @@ def updateCerts(request):
     microsoft_update()
     mozilla_update()
     return HttpResponse("Certs Updated!")
+
+
+def homePage(request):
+    return render(request, "storeviewer/homepage.html",
+            context={
+                "rootstores": Root_Store.objects.all()
+                })
+
+
+
+def viewStore(request, name):
+    return render(request, "storeviewer/rootstore.html",
+            context={
+                "store": Root_Store.objects.get(name=name)
+                })
+
+
+def viewCert(request, sha256):
+    return render(request, "storeviewer/certificate.html",
+            context={
+                "cert": Certificate.objects.get(sha256=sha256)
+                })
