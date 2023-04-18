8a4a6178515d1bb3179f1d76e0b65dff40d13566
Jacob Adams <jacob@tookmund.com>
2023-04-18 17:51:43 -0400

    Fix models to include relationship data

--- storeviewer/models.py
+++ storeviewer/models.py
@@ -6,11 +6,25 @@ class Certificate(models.Model):
     sha256 = models.CharField(max_length=64, unique=True)
     pem = models.CharField(max_length=16384)
     common_name = models.CharField(max_length=512)
-    stores = models.ManyToManyField("Root_Store")
+    stores = models.ManyToManyField("Root_Store", through="Store_Contents")
 
 
 class Root_Store(models.Model):
     name = models.CharField(max_length=512, unique=True)
     source = models.URLField(max_length=1000)
-    certificates = models.ManyToManyField(Certificate)
+    certificates = models.ManyToManyField(Certificate, through="Store_Contents")
 
+
+class Store_Contents(models.Model):
+    certificate = models.ForeignKey(Certificate, on_delete=models.CASCADE)
+    root_store = models.ForeignKey(Root_Store, on_delete=models.CASCADE)
+    active = models.BooleanField()
+    last_trusted = models.DateTimeField()
+
+    class Meta:
+        constraints = [
+                models.UniqueConstraint(
+                    fields=["certificate", "root_store"],
+                    name="unique_migration_host_combination"
+                )
+        ]
