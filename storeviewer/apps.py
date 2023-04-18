c967fe12fa12dccfa0cd7f3b91beee8effe461e3
Jacob Adams <jacob@tookmund.com>
2023-04-17 23:01:22 -0400

    Reformat with black

--- storeviewer/apps.py
+++ storeviewer/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StoreviewerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'storeviewer'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "storeviewer"
--- storeviewer/urls.py
+++ storeviewer/urls.py
@@ -5,4 +5,3 @@ from .views import homePageView
 urlpatterns = [
     path("", homePageView, name="home"),
 ]
-
