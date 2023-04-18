e8fb6ad11273e69899f912faaeb3702236637a41
Jacob Adams <jacob@tookmund.com>
2023-04-17 22:22:08 -0400

    initial django app

--- rootstore/settings.py
+++ rootstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9%+x*w&$81nvztl7pg()4ujf7brhrkx53i&my6ufkl-*vlk)8g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storeviewer'
 ]
 
 MIDDLEWARE = [
