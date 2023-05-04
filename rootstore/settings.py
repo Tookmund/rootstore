1fa3250ccdad5572809557772335bb3c68e726f1
Jacob Adams <jacob@tookmund.com>
2023-05-03 22:26:21 -0400

    settings: Set DEBUG if passed in the environment

--- rootstore/settings.py
+++ rootstore/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9%+x*w&$81nvztl7pg()4ujf7brhrkx53i&my6ufkl-*vlk)8g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") else False
 
 ALLOWED_HOSTS = ["*"]
 
